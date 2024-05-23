80fd672353589709cface2d82bfe630e799742c7
John Bowser <john.bow7720@gmail.com>
2024-05-22 23:24:30 -0400

    update usage text on error

--- Program.cs
+++ Program.cs
@@ -5,4 +5,4 @@ Parser.Default
   .ParseArguments<RenderOptions, FontListOptions>(args)
   .WithParsed<RenderOptions>(AsciiArt.Write)
   .WithParsed<FontListOptions>(FontList.Write)
-  .WithNotParsed(_ => WriteLine("Usage: HelloDotNet <text> --font Big"));
+  .WithNotParsed(_ => WriteLine("Usage: `HelloDotNet <text> --font Big` or `HelloDotNet list`"));
