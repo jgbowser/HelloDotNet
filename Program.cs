fe4d8b0d881d2e614c9d2f3eb5b72daffc3fb1ca
John Bowser <john.bow7720@gmail.com>
2024-05-23 22:42:16 -0400

    move gitognore back to top level

--- Program.cs
+++
@@ -1,8 +0,0 @@
-﻿using HelloDotNet;
-using CommandLine;
-
-Parser.Default
-  .ParseArguments<RenderOptions, FontListOptions>(args)
-  .WithParsed<RenderOptions>(AsciiArt.Write)
-  .WithParsed<FontListOptions>(FontList.Write)
-  .WithNotParsed(_ => WriteLine("Usage: `HelloDotNet <text> --font Big` or `HelloDotNet list`"));
